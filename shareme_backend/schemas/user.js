d61ae2ce8a649a7434ccf057e92d45444d6a7eb9
ChandanSahu <chandangupta9811@gmail.com>
2023-09-09 22:09:37 +0530

    feat: CMS integrated

---
+++ shareme_backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'user',
+  title: 'User',
+  type: 'document',
+  fields: [
+    {
+      name: 'userName',
+      title: 'UsernName',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    },
+  ],
+}
