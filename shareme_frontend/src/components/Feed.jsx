c71ae4951070923897fb166035c5db926743be75
ChandanSahu <chandangupta9811@gmail.com>
2023-09-10 22:02:12 +0530

    feat: pin showing up to feed

--- shareme_frontend/src/components/Feed.jsx
+++ shareme_frontend/src/components/Feed.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { client } from "../client";
 import { useParams } from "react-router-dom";
 import { Spinner, MasonryLayout } from "./index";
+import { feedQuery, searchQuery } from "../utils/data";
 
 const Feed = () => {
   const [loading, setLoading] = useState(false);
-  const params = useParams();
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams();
+
+  useEffect(() => {
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
 
   if (loading)
     return <Spinner message="We are adding new ideas to your feed!" />;
 
-  return <div>Feed</div>;
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
 };
 
 export default Feed;
--- shareme_frontend/src/components/index.js
+++ shareme_frontend/src/components/index.js
@@ -8,3 +8,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Search } from "./Search";
 export { default as MasonryLayout } from "./MasonryLayout";
 export { default as Spinner } from "./Spinner";
+export { default as Pin } from "./Pin";
