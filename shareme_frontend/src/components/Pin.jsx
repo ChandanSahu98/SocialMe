5a15f36071ba08f2e4e50fcd96784cd2b5767fea
ChandanSahu <chandangupta9811@gmail.com>
2023-09-13 22:28:09 +0530

    feat: added create pin component

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -14,7 +14,6 @@ const Pin = ({ pin }) => {
   const navigate = useNavigate();
 
   const { postedBy, image, _id, destination } = pin;
-  console.log(pin);
 
   const user =
     localStorage.getItem("user") !== "undefined"
