35219c6e3619d113fd4f1c4478a237601c00b433
ChandanSahu <chandangupta9811@gmail.com>
2023-09-18 21:48:49 +0530

    feat: user login

--- shareme_frontend/src/App.jsx
+++ shareme_frontend/src/App.jsx
@@ -1,8 +1,19 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./container/Home";
+import { useEffect } from "react";
+import { fetchUser } from "./utils/fetchUser";
 
 const App = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
